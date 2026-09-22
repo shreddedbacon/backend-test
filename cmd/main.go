4f955faaa4594a096b458f5fd084436467c557f1
shreddedbacon <shreddedbacon@users.noreply.github.com>
2026-09-22 13:52:53 +1000

    chore: golang backend for fun

--- cmd/main.go
+++ cmd/main.go
@@ -4,39 +4,48 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	lagoonProject = os.Getenv("LAGOON_PROJECT")
+	lagoonEnvironment = os.Getenv("LAGOON_ENVIRONMENT")
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/cms", cmsHandler)
 	http.HandleFunc("/api", apiHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
+var lagoonProject, lagoonEnvironment string
+
 var page = `<html>
 <body bgcolor="#4c7ab0">
 <h1 style="color:#000000">Hello world Backend!</h1>
 </body>
-</html>`
+</html>\n`
 
 var cms = `<html>
 <body bgcolor="#56b54e">
 <h1 style="color:#000000">CMS</h1>
+<h1 style="color:#000000">Project: %s</h1>
+<h1 style="color:#000000">Environment: %s</h1>
 </body>
-</html>`
+</html>\n`
 
 var api = `<html>
 <body bgcolor="#fcba03">
 <h1 style="color:#000000">API</h1>
+<h1 style="color:#000000">Project: %s</h1>
+<h1 style="color:#000000">Environment: %s</h1>
 </body>
-</html>`
+</html>\n`
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(fmt.Sprintf("%v\n", page)))
 }
 func cmsHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(fmt.Sprintf("%v\n", cms)))
+	w.Write([]byte(fmt.Sprintf(cms, lagoonProject, lagoonEnvironment)))
 }
 func apiHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(fmt.Sprintf("%v\n", api)))
+	w.Write([]byte(fmt.Sprintf(api, lagoonProject, lagoonEnvironment)))
 }
